c2ae63be036370295bc2e84b3f70d35454e36d43
Avi Shmueli <Avi.Shmueli@Briefcam.com>
2020-06-18 06:25:31 +0000

    d

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import { render } from "react-dom";
 import App from "./components/App";
 
 render(<App />, document.getElementById("root"));
+
+ // https://online.visualstudio.com/environment/a2ff65ea-d7bb-4eff-84ea-004b3393a61b
